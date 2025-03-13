514a3ec4a5b7f4391497ee3abc279873b21cafa8
nopparat-tk <nopparat.tk@gmail.com>
2025-03-13 17:11:10 +0700

    update FacebookMSG

--- components/elements/FacebookMSG.js
+++ components/elements/FacebookMSG.js
@@ -1,38 +1,39 @@
 "use client";
-import { useEffect } from "react";
+import React, { useState } from "react";
+import Link from "next/link";
+import "../../public/assets/css/module-css/chat-button.css";
 
 export default function FacebookMSG() {
-   useEffect(() => {
-      console.log("Initializing Facebook SDK");
-      window.fbAsyncInit = function () {
-         FB.init({
-            appId: "1278743569993286",
-            xfbml: true,
-            version: "v18.0",
-         });
-         console.log("Facebook SDK initialized");
-      };
+   const [isActivate, setActivate] = useState(false);
 
-      (function (d, s, id) {
-         var js,
-            fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) return;
-         js = d.createElement(s);
-         js.id = id;
-         js.src =
-            "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v18.0&appId=1278743569993286";
-         fjs.parentNode.insertBefore(js, fjs);
-         console.log("Facebook SDK script added to the document");
-      })(document, "script", "facebook-jssdk");
-   }, []);
+   const handleActivate = () => {
+      setActivate(!isActivate);
+   };
 
    return (
-      <div>
-         <div
-            className="fb-customerchat"
-            attribution="setup_tool"
-            page_id="207440262444547"
-         ></div>
+      <div className="chatContainer">
+         <button onClick={handleActivate} className="chatButton">
+            {isActivate ? (
+               <i className="fas fa-times close"></i>
+            ) : (
+               <i className="fa fa-comments"></i>
+            )}
+         </button>
+         {isActivate && (
+            <div className="iconConatiner">
+               <Link
+                  className="fab fa-facebook-f msgIcon"
+                  href="m.me/207440262444547"
+                  target="_blank"
+               ></Link>
+
+               <Link
+                  className="fab fa-line lineIcon"
+                  href="https://lin.ee/Vwy9xO6"
+                  target="_blank"
+               ></Link>
+            </div>
+         )}
       </div>
    );
 }
