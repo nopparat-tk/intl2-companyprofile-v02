d994c24bc5cc78c4ec7068cecbe50d3129a9cc9b
nopparat-tk <105415402+nopparat-tk@users.noreply.github.com>
2025-08-08 10:51:40 +0700

    Update layout.js for gclid

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -5,27 +5,39 @@ import "swiper/css/pagination";
 import "swiper/css/free-mode";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { notoSans, notoSansTh, notoSansSC } from "@/lib/font";
-import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
-import CookiesConsent from "@/components/elements/CookiesConsent";
-import FacebookMSG from "@/components/elements/FacebookMSG";
-import { getTranslations } from "next-intl/server";
-import GclidScript from "@/components/elements/GclidScript";
+import { getMessages, getTranslations } from "next-intl/server";
+import dynamic from "next/dynamic";
 
-export async function generateMetadata() {
-  const t = await getTranslations("metaData");
+const CookiesConsent = dynamic(
+  () => import("@/components/elements/CookiesConsent"),
+  { ssr: false }
+);
+const FacebookMSG = dynamic(() => import("@/components/elements/FacebookMSG"), {
+  ssr: false,
+});
+const GclidScript = dynamic(() => import("@/components/elements/GclidScript"), {
+  ssr: false,
+});
+
+export async function generateMetadata({ params: { locale } }) {
+  const t = await getTranslations({ locale, namespace: "metaData" });
   return {
     title: t("title"),
     description: t("desc"),
   };
 }
 
-export default async function LocaleLayout({ children, params }) {
-  const { locale } = await params;
-  if (!hasLocale(routing.locales, locale)) {
+export default async function LocaleLayout({ children, params: { locale } }) {
+  // Validate that the incoming `locale` parameter is valid
+  if (!routing.locales.includes(locale)) {
     notFound();
   }
+
+  // Providing all messages to the client side is the easiest way to get started
+  const messages = await getMessages();
 
   const fontClass =
     locale === "th"
@@ -37,8 +49,8 @@ export default async function LocaleLayout({ children, params }) {
   return (
     <html lang={locale} className={fontClass}>
       <body>
-        <GoogleTagManager gtmId="GTM-KF74GNH5" />
-        <NextIntlClientProvider locale={locale}>
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
+        <NextIntlClientProvider locale={locale} messages={messages}>
           <FacebookMSG />
           <CookiesConsent />
           {children}
